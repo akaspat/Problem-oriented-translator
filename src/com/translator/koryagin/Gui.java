f86ee097874c829547f47010f9e4442deea040ce
akaspat <akaspat@mail.ru>
2020-11-18 13:16:25 +0300

    add action on button click

--- src/com/translator/koryagin/Gui.java
+++ src/com/translator/koryagin/Gui.java
@@ -2,6 +2,12 @@ package com.translator.koryagin;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Gui {
@@ -9,6 +15,8 @@ public class Gui {
     private JTextArea inputAreaText;
     private JTextArea outputAreaText;
     private JButton buildBtn;
+    private ButtonActionListener btnListener;
+    private List<String> program;
 
     public Gui() {
         frame = new JFrame();
@@ -23,7 +31,7 @@ public class Gui {
      */
     private void buildWindow(){
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1400, 1000);
+        frame.setSize(700, 500);
 
         // INPUT AREA
         // create and place input area on the left side
@@ -54,6 +62,8 @@ public class Gui {
 
         // BUILD BTN
         buildBtn = new JButton("Build");
+        btnListener = new ButtonActionListener();
+        buildBtn.addActionListener(btnListener);
         buildBtn.setFont(new Font("serif", Font.BOLD, 40));
         JPanel buildArea = new JPanel();
         buildArea.add(buildBtn);
@@ -61,10 +71,120 @@ public class Gui {
     }
 
     public void setInputAreaText(List<String> lines, boolean clearPrevious){
-        if (!clearPrevious) inputAreaText.removeAll();
+        if (clearPrevious) inputAreaText.removeAll();
         for (String line : lines){
             inputAreaText.append(line);
             inputAreaText.append("\n");
+        }
+    }
+
+    public void parseProgramTextToPath(boolean fromFile){
+        // get string from file
+        if (fromFile) {
+            Path pathInputProgram = Path.of("files/program");
+            program = null;
+            try {
+                program = Files.readAllLines(pathInputProgram);
+                setInputAreaText(program, true);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        // get string from gui input area
+        else {
+            String[] lines = inputAreaText.getText().split("\n");
+            program.clear();
+            for (String line : lines) { program.add(line); }
+        }
+
+    }
+
+    public void checkAllProgram() {
+        /*
+         check every line
+         see program struct in files/program
+        */
+
+        // check start
+        String start = program.get(0);
+        if (!start.equals("\"Программа\"")){
+            System.out.println("Ошибка. В начале ожидалось \"Программа\"");
+        }
+
+        program.remove(0);
+
+        // check header
+        String header = program.get(0);
+        String[] header_elements = header.split(" ");
+        if (!header_elements[0].equals("\"Метки\"")){
+            System.out.println("Ошибка. Ожидалось слово \"Метки\"");
+        }
+
+        // check every sign in header
+        Sign sign_checker = new Sign();
+        for (int i=1; i < header_elements.length; i++){
+            sign_checker.setSignString(header_elements[i]);
+            sign_checker.isCorrect(true);
+        }
+        program.remove(0);
+
+        // collect operators
+        ArrayList<String> operators = new ArrayList<>();
+        String oper = program.get(0);
+        while ( !oper.equals("\"Конец программы\"") ){
+            operators.add(oper);
+            program.remove(0);
+
+            oper = program.get(0);
+        }
+
+        for (String _operator : operators) {
+            String[] elements = _operator.split(" ");
+
+            // get MInteger (метка = цел)
+            MInteger metka = new MInteger(elements[0]);
+            metka.isCorrect(true);
+
+            // get ":"
+            if (!elements[1].equals(":")){
+                System.out.println(String.format("Ошибка. Ожидалось двоеточние вместо %s", elements[1]));
+            }
+
+            // get Variable (пер)
+            Variable var = new Variable(elements[2]);
+            var.isCorrect(true);
+
+            // get "="
+            if (!elements[3].equals("=")){
+                System.out.println(String.format("Ошибка. Ожидался \"=\" вместо %s", elements[3]));
+            }
+
+            // get CalculatePart (прав.часть)
+            String calPratString = "";
+            for (int i=4; i < elements.length; i++) { calPratString += elements[i]; }
+            CalculatePart cp = new CalculatePart(calPratString);
+            cp.isCorrect(true);
+        }
+
+        // check ends
+        // get index of last element
+        int end = program.size() - 1;
+        String end_str = program.get(end);
+        if (!end_str.equals("\"Конец программы\"")){
+            System.out.println("Ошибка. В конце ожидалось \"Конец программы\"");
+        }
+        program.remove(end);
+
+        if (!program.isEmpty()){
+            System.out.println("Откуда о_О");
+        }
+    }
+
+    public class ButtonActionListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            parseProgramTextToPath(false);
+            checkAllProgram();
         }
     }
 }
--- src/com/translator/koryagin/Main.java
+++ src/com/translator/koryagin/Main.java
@@ -7,96 +7,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static List<String> program;
+
     public static void main(String[] args) {
         Gui g = new Gui();
-
-        // show program text on gui
-        Path pathInputProgram = Path.of("files/program");
-        List<String> program = null;
-        try {
-            program = Files.readAllLines(pathInputProgram);
-            g.setInputAreaText(program, true);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        /*
-         check every line
-         see program struct in files/program
-        */
-
-        // check start
-        String start = program.get(0);
-        if (!start.equals("\"Программа\"")){
-            System.out.println("Ошибка. В начале ожидалось \"Программа\"");
-        }
-        program.remove(0);
-
-        // check header
-        String header = program.get(0);
-        String[] header_elements = header.split(" ");
-        if (!header_elements[0].equals("\"Метки\"")){
-            System.out.println("Ошибка. Ожидалось слово \"Метки\"");
-        }
-
-        // check every sign in header
-        Sign sign_checker = new Sign();
-        for (int i=1; i < header_elements.length; i++){
-            sign_checker.setSignString(header_elements[i]);
-            sign_checker.isCorrect(true);
-        }
-        program.remove(0);
-
-        // collect operators
-        ArrayList<String> operators = new ArrayList<>();
-        int index = 0;
-        String oper = program.get(index);
-        while ( !oper.equals("\"Конец программы\"") ){
-            operators.add(oper);
-            index++;
-            oper = program.get(index);
-        }
-
-        // TODO: check operators
-        for (String _operator : operators) {
-            String[] elements = _operator.split(" ");
-
-            // get MInteger (метка = цел)
-            MInteger metka = new MInteger(elements[0]);
-            metka.isCorrect(true);
-
-            // get ":"
-            if (!elements[1].equals(":")){
-                System.out.println(String.format("Ошибка. Ожидалось двоеточние вместо %s", elements[1]));
-            }
-
-            // get Variable (пер)
-            Variable var = new Variable(elements[2]);
-            var.isCorrect(true);
-
-            // get "="
-            if (!elements[3].equals("=")){
-                System.out.println(String.format("Ошибка. Ожидался \"=\" вместо %s", elements[3]));
-            }
-
-            // get CalculatePart (прав.часть)
-            String calPratString = "";
-            for (int i=4; i < elements.length; i++) { calPratString += elements[i]; }
-            CalculatePart cp = new CalculatePart(calPratString);
-            cp.isCorrect(true);
-        }
-
-        // check ends
-        // get index of last element
-        int end = program.size() - 1;
-        String end_str = program.get(end);
-        if (!end_str.equals("\"Конец программы\"")){
-            System.out.println("Ошибка. В конце ожидалось \"Конец программы\"");
-        }
-        program.remove(end);
-
-        if (!program.isEmpty()){
-            System.out.println("Откуда о_О");
-        }
+        g.parseProgramTextToPath(true);
     }
 }
